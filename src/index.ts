794e4b15b94db35871b1b8cbb5be37b959f6a26e
narspi <ivanovmichurina@gmail.com>
2025-08-29 13:40:31 +0300

    add loader node

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import express from "express";
-import cors from "cors";
-import logger from "morgan";
-import path from 'path';
-import {fileURLToPath} from 'url';
+    import express from "express";
+    import cors from "cors";
+    import logger from "morgan";
+    import path from 'path';
+    import {fileURLToPath} from 'url';
+    import streamRouter from "./modules/stream/stream.controler";
+    import contentRouter from "./modules/content/content.controler";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-import streamRouter from "./modules/stream/stream.controler.ts";
-import contentRouter from "./modules/content/content.controler.ts";
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(logger('dev'));
+    app.set('views',path.join(__dirname, 'views'));
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(logger('dev'));
-app.set('views',path.join(__dirname, 'views'));
+    app.use('/content',contentRouter);
+    app.use('/stream',streamRouter)
 
-app.use('/content',contentRouter);
-app.use('/stream',streamRouter)
+    const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT,()=>{
-    console.log('start ')
-})
+    app.listen(PORT,()=>{
+        console.log('start ')
+    })
